94ce6b9fffb305af5671082bd2b785d53f29e0f1
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2023-11-18 22:44:16 -0300

    [first] Criação da base do projeto

---
+++ src/main/java/br/com/cliniv/emailapi/EMailApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cliniv.emailapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMailApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cliniv/emailapi/EMailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cliniv.emailapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
